f2932825aa913e5e88824ef82a46e4071869851b
takahiro-tech <56814714+takahiro-tech@users.noreply.github.com>
2019-10-29 20:50:59 +0900

    edit view

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,15 @@
 class Group < ApplicationRecord
   has_many :group_users
   has_many :users, through: :group_users
-  validates :groupname, presence: true, uniqueness: true
   has_many :messages
+
+  validates :groupname, presence: true, uniqueness: true
+ 
+  def show_last_message
+    if (last_message = messages.last).present?
+      last_message.body? ? last_message.body : '画像が投稿されています'
+    else
+      'まだメッセージはありません。'
+    end
+  end
 end
